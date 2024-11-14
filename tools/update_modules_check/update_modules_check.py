1cee43708c0051787d8ccdb287a5e3c5d261f1bc
zhangdonghao <39961809+hawk9821@users.noreply.github.com>
2024-11-14 20:26:15 +0800

    [Fix][CI] time-consuming module optimization (#8052)

--- tools/update_modules_check/update_modules_check.py
+++ tools/update_modules_check/update_modules_check.py
@@ -151,6 +151,9 @@ def get_sub_it_modules(modules, total_num, current_num):
     modules_arr.remove("connector-doris-e2e")
     modules_arr.remove("connector-paimon-e2e")
     modules_arr.remove("connector-cdc-oracle-e2e")
+    modules_arr.remove("connector-file-local-e2e")
+    modules_arr.remove("connector-file-sftp-e2e")
+    modules_arr.remove("connector-redis-e2e")
     output = ""
     for i, module in enumerate(modules_arr):
         if len(module) > 0 and i % int(total_num) == int(current_num):
@@ -186,6 +189,12 @@ def get_sub_update_it_modules(modules, total_num, current_num):
         module_list.remove("connector-paimon-e2e")
     if "connector-cdc-oracle-e2e" in module_list:
         module_list.remove("connector-cdc-oracle-e2e")
+    if "connector-file-local-e2e" in module_list:
+        module_list.remove("connector-file-local-e2e")
+    if "connector-file-sftp-e2e" in module_list:
+        module_list.remove("connector-file-sftp-e2e")
+    if "connector-redis-e2e" in module_list:
+        module_list.remove("connector-redis-e2e")
     for i, module in enumerate(module_list):
         if len(module) > 0 and i % int(total_num) == int(current_num):
             final_modules.append(":" + module)
