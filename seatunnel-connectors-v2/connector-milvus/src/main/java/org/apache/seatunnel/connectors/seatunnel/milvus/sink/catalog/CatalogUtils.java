f17da3210bcc7ea35a66574eb7aa331dbaa23e9c
Nian Liu <nian.liu@zilliz.com>
2025-03-10 11:04:14 +0800

    add auto id when there are no primary key or more than one pk

--- seatunnel-connectors-v2/connector-milvus/src/main/java/org/apache/seatunnel/connectors/seatunnel/milvus/sink/catalog/CatalogUtils.java
+++ seatunnel-connectors-v2/connector-milvus/src/main/java/org/apache/seatunnel/connectors/seatunnel/milvus/sink/catalog/CatalogUtils.java
@@ -63,113 +63,115 @@ public class CatalogUtils {
     }
 
     void createTableInternal(TablePath tablePath, CatalogTable catalogTable) {
-        try {
-            Map<String, String> options = catalogTable.getOptions();
+        Map<String, String> options = catalogTable.getOptions();
 
-            // partition key logic
-            String partitionKeyField = null;
-            if(options.containsKey(MilvusOptions.PARTITION_KEY_FIELD)){
-                partitionKeyField = options.get(MilvusOptions.PARTITION_KEY_FIELD);
-            }
-            // if partition key is set in config, use the one in config
-            if (StringUtils.isNotEmpty(config.get(MilvusSinkConfig.PARTITION_KEY))) {
-                partitionKeyField = config.get(MilvusSinkConfig.PARTITION_KEY);
-            }
+        // partition key logic
+        String partitionKeyField = null;
+        if(options.containsKey(MilvusOptions.PARTITION_KEY_FIELD)){
+            partitionKeyField = options.get(MilvusOptions.PARTITION_KEY_FIELD);
+        }
+        // if partition key is set in config, use the one in config
+        if (StringUtils.isNotEmpty(config.get(MilvusSinkConfig.PARTITION_KEY))) {
+            partitionKeyField = config.get(MilvusSinkConfig.PARTITION_KEY);
+        }
 
-            TableSchema tableSchema = catalogTable.getTableSchema();
-            List<CreateCollectionReq.FieldSchema> fieldSchemaList = new ArrayList<>();
-            Boolean enableAutoId = false;
-            if(options.containsKey(MilvusOptions.ENABLE_AUTO_ID)){
-                enableAutoId = Boolean.valueOf(options.get(MilvusOptions.ENABLE_AUTO_ID));
-            }
-            if(config.get(MilvusSinkConfig.ENABLE_AUTO_ID) != null){
-                enableAutoId = config.get(MilvusSinkConfig.ENABLE_AUTO_ID);
-            }
-            for (Column column : tableSchema.getColumns()) {
-                if (column.getOptions() != null
-                        && column.getOptions().containsKey(CommonOptions.METADATA.getName())
-                        && (Boolean) column.getOptions().get(CommonOptions.METADATA.getName())) {
-                    // skip dynamic field
-                    continue;
-                }
-                CreateCollectionReq.FieldSchema fieldSchema = MilvusSinkConverter.convertToFieldType(
-                        column,
-                        tableSchema.getPrimaryKey(),
-                        partitionKeyField,
-                        enableAutoId);
-                fieldSchemaList.add(fieldSchema);
-            }
-
-            Boolean enableDynamicField = true;
-
-            if(options.containsKey(MilvusOptions.ENABLE_DYNAMIC_FIELD)) {
-                enableDynamicField = Boolean.valueOf(options.get(MilvusOptions.ENABLE_DYNAMIC_FIELD));
-            }
-            // if enable_dynamic_field is set in config, use the one in config
-            if(config.get(ENABLE_DYNAMIC_FIELD) != null){
-                enableDynamicField = config.get(ENABLE_DYNAMIC_FIELD);
-            }
-
-            // consistency level
-            ConsistencyLevel consistencyLevel = ConsistencyLevel.BOUNDED;
-            if(options.containsKey(MilvusOptions.CONSISTENCY_LEVEL)){
-                consistencyLevel = ConsistencyLevel.valueOf(options.get(MilvusOptions.CONSISTENCY_LEVEL).toUpperCase());
-            }
-            if(config.get(MilvusSinkConfig.CONSISTENCY_LEVEL) != null){
-                consistencyLevel = config.get(MilvusSinkConfig.CONSISTENCY_LEVEL);
-            }
-
-            String collectionDescription = "";
-            if (config.get(MilvusSinkConfig.COLLECTION_DESCRIPTION) != null
-                    && config.get(MilvusSinkConfig.COLLECTION_DESCRIPTION)
-                    .containsKey(tablePath.getTableName())) {
-                // use description from config first
-                collectionDescription =
-                        config.get(MilvusSinkConfig.COLLECTION_DESCRIPTION)
-                                .get(tablePath.getTableName());
-            } else if (null != catalogTable.getComment()) {
-                collectionDescription = catalogTable.getComment();
-            }
-            CreateCollectionReq.CollectionSchema collectionSchema = CreateCollectionReq.CollectionSchema.builder()
-                    .fieldSchemaList(fieldSchemaList)
-                    .enableDynamicField(enableDynamicField)
-                    .build();
-            CreateCollectionReq createCollectionReq =
-                    CreateCollectionReq.builder()
-                            .collectionName(tablePath.getTableName())
-                            .description(collectionDescription)
-                            .collectionSchema(collectionSchema)
-                            .enableDynamicField(enableDynamicField)
-                            .consistencyLevel(consistencyLevel)
+        TableSchema tableSchema = catalogTable.getTableSchema();
+        List<CreateCollectionReq.FieldSchema> fieldSchemaList = new ArrayList<>();
+        Boolean enableAutoId = false;
+        if(options.containsKey(MilvusOptions.ENABLE_AUTO_ID)){
+            enableAutoId = Boolean.valueOf(options.get(MilvusOptions.ENABLE_AUTO_ID));
+        }
+        if(config.get(MilvusSinkConfig.ENABLE_AUTO_ID) != null){
+            enableAutoId = config.get(MilvusSinkConfig.ENABLE_AUTO_ID);
+        }
+        if((tableSchema.getPrimaryKey() == null || tableSchema.getPrimaryKey().getColumnNames().size() > 1) && enableAutoId){
+            CreateCollectionReq.FieldSchema fieldSchema = CreateCollectionReq.FieldSchema.builder()
+                            .name("auto_id")
+                            .isPrimaryKey(true)
+                            .dataType(DataType.Int64)
                             .build();
-            if (StringUtils.isNotEmpty(options.get(MilvusOptions.SHARDS_NUM))) {
-                createCollectionReq.setNumShards(Integer.parseInt(options.get(MilvusOptions.SHARDS_NUM)));
+            fieldSchemaList.add(fieldSchema);
+        }
+        for (Column column : tableSchema.getColumns()) {
+            if (column.getOptions() != null
+                    && column.getOptions().containsKey(CommonOptions.METADATA.getName())
+                    && (Boolean) column.getOptions().get(CommonOptions.METADATA.getName())) {
+                // skip dynamic field
+                continue;
             }
-            if(config.get(MilvusSinkConfig.SHARDS_NUM) != null){
-                createCollectionReq.setNumShards(config.get(MilvusSinkConfig.SHARDS_NUM));
-            }
-            int retry = 5;
-            while (retry > 0){
+            CreateCollectionReq.FieldSchema fieldSchema = MilvusSinkConverter.convertToFieldType(
+                    column,
+                    tableSchema.getPrimaryKey(),
+                    partitionKeyField,
+                    enableAutoId);
+            fieldSchemaList.add(fieldSchema);
+        }
+
+        Boolean enableDynamicField = true;
+
+        if(options.containsKey(MilvusOptions.ENABLE_DYNAMIC_FIELD)) {
+            enableDynamicField = Boolean.valueOf(options.get(MilvusOptions.ENABLE_DYNAMIC_FIELD));
+        }
+        // if enable_dynamic_field is set in config, use the one in config
+        if(config.get(ENABLE_DYNAMIC_FIELD) != null){
+            enableDynamicField = config.get(ENABLE_DYNAMIC_FIELD);
+        }
+
+        // consistency level
+        ConsistencyLevel consistencyLevel = ConsistencyLevel.BOUNDED;
+        if(options.containsKey(MilvusOptions.CONSISTENCY_LEVEL)){
+            consistencyLevel = ConsistencyLevel.valueOf(options.get(MilvusOptions.CONSISTENCY_LEVEL).toUpperCase());
+        }
+        if(config.get(MilvusSinkConfig.CONSISTENCY_LEVEL) != null){
+            consistencyLevel = config.get(MilvusSinkConfig.CONSISTENCY_LEVEL);
+        }
+
+        String collectionDescription = "";
+        if (config.get(MilvusSinkConfig.COLLECTION_DESCRIPTION) != null
+                && config.get(MilvusSinkConfig.COLLECTION_DESCRIPTION)
+                .containsKey(tablePath.getTableName())) {
+            // use description from config first
+            collectionDescription =
+                    config.get(MilvusSinkConfig.COLLECTION_DESCRIPTION)
+                            .get(tablePath.getTableName());
+        } else if (null != catalogTable.getComment()) {
+            collectionDescription = catalogTable.getComment();
+        }
+        CreateCollectionReq.CollectionSchema collectionSchema = CreateCollectionReq.CollectionSchema.builder()
+                .fieldSchemaList(fieldSchemaList)
+                .enableDynamicField(enableDynamicField)
+                .build();
+        CreateCollectionReq createCollectionReq =
+                CreateCollectionReq.builder()
+                        .collectionName(tablePath.getTableName())
+                        .description(collectionDescription)
+                        .collectionSchema(collectionSchema)
+                        .enableDynamicField(enableDynamicField)
+                        .consistencyLevel(consistencyLevel)
+                        .build();
+        if (StringUtils.isNotEmpty(options.get(MilvusOptions.SHARDS_NUM))) {
+            createCollectionReq.setNumShards(Integer.parseInt(options.get(MilvusOptions.SHARDS_NUM)));
+        }
+        if(config.get(MilvusSinkConfig.SHARDS_NUM) != null){
+            createCollectionReq.setNumShards(config.get(MilvusSinkConfig.SHARDS_NUM));
+        }
+        int retry = 5;
+        while (retry > 0){
+            try {
+                client.createCollection(createCollectionReq);
+                break;
+            } catch (Exception e) {
+                log.error("create collection failed, retry: {}", retry);
+                retry--;
+                if(retry == 0){
+                    throw new MilvusConnectorException(MilvusConnectionErrorCode.CREATE_COLLECTION_ERROR, e.getMessage());
+                }
                 try {
-                    client.createCollection(createCollectionReq);
-                    break;
-                } catch (Exception e) {
-                    log.error("create collection failed, retry: {}", retry);
-                    retry--;
-                    if(retry == 0){
-                        throw new MilvusConnectorException(MilvusConnectionErrorCode.CREATE_COLLECTION_ERROR, e.getMessage());
-                    }
-                    try {
-                        TimeUnit.SECONDS.sleep(1);
-                    } catch (InterruptedException interruptedException) {
-                        log.error("sleep failed", interruptedException);
-                    }
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException interruptedException) {
+                    log.error("sleep failed", interruptedException);
                 }
             }
-
-        } catch (Exception e) {
-            throw new MilvusConnectorException(
-                    MilvusConnectionErrorCode.CREATE_COLLECTION_ERROR, e);
         }
     }
 }
